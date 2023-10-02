f423daa0f65c7715024e3d09b8e14787cf17a6a2
35kartal35 <mozdemir3545@gmail.com>
2023-10-02 22:12:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Benim Uzun bir aradan sonra kurduğum yeni projem <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mehmet Özdemir
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
